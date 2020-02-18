056dbe5e538ae18e68908af88997f22347f6c0d5
Kevin <kevin@Kevins-MacBook-Pro.local>
2020-02-18 18:42:20 -0500

    Rename question numbers on holder and PageSnap addition

--- app/src/main/java/com/android/bedsidechats/fragments/CardsFragment.java
+++ app/src/main/java/com/android/bedsidechats/fragments/CardsFragment.java
@@ -14,6 +14,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
+import androidx.recyclerview.widget.PagerSnapHelper;
 
 import com.android.bedsidechats.R;
 //import com.android.bedsidechats.data.CardAdapter;
@@ -132,6 +134,8 @@ public class CardsFragment extends Fragment implements View.OnClickListener {
                             Log.d(TAG, "" + mQuestionMap);
                             mAdapter = new CardAdapter(getActivity(), mQuestionMap, getFragmentManager(), mLanguageChoice, mProviderChoice);
                             mCards.setAdapter(mAdapter);
+                            SnapHelper snapHelper = new PagerSnapHelper();
+                            snapHelper.attachToRecyclerView(mCards);
                             Log.d(TAG, task.getResult().getId() + " => " + task.getResult().getData());
                         }
                         else {
